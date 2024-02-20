fb2c6a79e23723c5c9eee6294d9064a41a8544b5
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-02-20 18:44:04 +0200

    update auth.js middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,10 +8,12 @@
  * Returns: None
  */
 
-export function ensureAuth(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    res.redirect("/");
-  }
-}
+module.exports = {
+  ensureAuth: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/");
+    }
+  },
+};
