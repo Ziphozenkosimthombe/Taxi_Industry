91439b6623fc4abd6b8c4c38b77db1246caaa282
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-02-20 12:21:13 +0200

    add comments in main.js routes folder

--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,13 @@ const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
 const { ensureAuth } = require("../middleware/auth");
 
+/**
+ * This code  represents a router module in an server.js application.
+ * It defines various routes for different functionalities such as authentication, home, and posts.
+ * The routes are associated with corresponding controller functions.
+ * The module exports the router object for use in the main application.
+ */
+
 //Main Routes
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
@@ -19,6 +26,6 @@ router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
-router.post("/signup", upload.single('file'), authController.postSignup);
+router.post("/signup", upload.single("file"), authController.postSignup);
 
 module.exports = router;
