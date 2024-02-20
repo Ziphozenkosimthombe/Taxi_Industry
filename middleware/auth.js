1de282adf91d77650d248451629ac310ca5fe0c8
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-02-20 12:04:49 +0200

    update auth.js file for middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,17 @@
-module.exports = {
-  ensureAuth: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    } else {
-      res.redirect("/");
-    }
+/**
+ * Function: ensureAuth
+ * Description: Middleware function to ensure that the user is authenticated.
+ * Parameters:
+ *   - req: The request object.
+ *   - res: The response object.
+ *   - next: The next middleware function.
+ * Returns: None
+ */
+
+export function ensureAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/");
   }
-};
+}
