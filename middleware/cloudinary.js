1ff83fca722a7805ede74a9b95e5b9ad44a676e8
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2024-02-20 12:09:11 +0200

    add comments to cloudinary.js middleware

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,5 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 
+/**
+ * This code snippet exports the Cloudinary configuration object.
+ * It requires the Cloudinary package and the dotenv package for environment variables.
+ * The Cloudinary configuration object is set with the cloud name, API key, and API secret
+ * from the environment variables defined in the .env file.
+ *
+ */
+
 require("dotenv").config({ path: "./config/.env" });
 
 cloudinary.config({
